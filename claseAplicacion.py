da7136be2d955485b85987b86fcdd9f387fb1bc1
santiagojofre47 <82518448+santiagojofre47@users.noreply.github.com>
2022-06-29 09:46:32 -0300

    Update claseAplicacion.py

--- claseAplicacion.py
+++ claseAplicacion.py
@@ -1,64 +1,57 @@
 from tkinter import *
-from tkinter import ttk,messagebox,font 
-import requests
+from tkinter import ttk, font, messagebox
 
 class Aplicacion():
     __ventana = None
-    __dolares = None
-    __pesos = None
+    __precioBase = None
+    __valor =  None
+
+    def calcular(self):
+        try:
+            val = float(self.__valor.get())
+            base = float(self.precioEntry.get())
+            self.precioIVA.set(base + (base * (val/100)))
+            self.IVA.set(base * (val/100))
+
+        except ValueError:
+            messagebox.showerror(title='Error de tipo',message='Debe ingresar un valor numérico')
+            self.precioEntry.set('')
+            self.precioEntry.focus()
 
     def __init__(self):
         self.__ventana = Tk()
-        self.__ventana.geometry('245x117')
-        self.__ventana.title('Conversor de moneda')
-
-        mainframe=Frame(self.__ventana,padx=8,pady=8,bg='#68c3bf')
-        mainframe.grid(column=0,row=0,sticky=(N,W,E,S))
-        mainframe.columnconfigure(0,weight=0)
-        mainframe.rowconfigure(0,weight=0)
-        mainframe['borderwidth']=2
-        mainframe['relief']='sunken'
-
-        fuente=font.Font(weight='bold',size=9,family="Helvetica")
-
-        self.__dolares=StringVar()
-        self.__pesos=StringVar()
-
-        self.__dolares.trace('w', self.convertir)
-        self.dolaresEntry=Entry(mainframe,textvariable=self.__dolares,width=10)
-        self.dolaresEntry.grid(column=1,row=0, sticky= W)
-        self.equivaleLbl=Label(mainframe,text='es equivalente a',bg='#52afaa',font=fuente).grid(column=0,row=1)
-        self.dolarLbl=Label(mainframe,text='dólares',bg='#52afaa',font=fuente).grid(column=2,row=0)
-        self.pesosLbl=Label(mainframe,text='pesos',bg='#52afaa',font=fuente).grid(column=2,row=1)
-        self.precioConvertidoLbl=Label(mainframe,textvariable=self.__pesos,bg='#52afaa',font=fuente).grid(column=1,row=1)
-        self.botonSalir=Button(mainframe,text="Salir",command=self.__ventana.destroy,bg='#c7773e',font=fuente).grid(column=2,row=2)
-
+        self.__ventana.geometry('341x300')
+        self.__ventana.title('Cálculo de IVA')
+        self.__ventana.resizable(0,0)
+        self.__precioBase = StringVar()
+        self.IVA = StringVar()
+        self.precioIVA = StringVar()
+        self.__valor = StringVar()
+        mainframe = Frame(self.__ventana)
+        mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+        mainframe.columnconfigure(0, weight=2)
+        mainframe.rowconfigure(0, weight=2)
+        mainframe['borderwidth'] = 2
+        mainframe['relief'] = 'sunken'
+        self.tituloLbl = Label(mainframe, text = "Cálculo de IVA", font = 'bold')
+        self.tituloLbl.grid(row = 1, column = 3, sticky = S)
+        self.precioLbl = Label(mainframe, text="Precio sin IVA:",font='bold').grid(row = 2, column = 2,sticky = W)
+        self.boton1 = ttk.Radiobutton(mainframe,text = 'IVA 21%', variable = self.__valor, value = 21)
+        self.boton1.grid(row = 3, column =2, sticky =W)
+        self.boton2 = ttk.Radiobutton(mainframe,text = 'IVA 10.5%', variable =self.__valor, value = 10.5)
+        self.boton2.grid(row = 4, column =2, sticky =W)
+        self.ivalbl = Label(mainframe, text="IVA:",font='bold').grid(row = 5, column = 2,sticky = W)
+        self.precioIVAlbl = Label(mainframe, text="Precio con IVA:",font='bold').grid(row = 6, column = 2,sticky = W)
+        self.precioEntry = Entry(mainframe, width= 7, textvariable = self.__precioBase)
+        self.precioEntry.grid(column=4, row=2, sticky=(W, E))
+        self.IVAEntry = Entry(mainframe, width= 7, textvariable = self.IVA)
+        self.IVAEntry.grid(column=4, row=5, sticky=(W, E))
+        self.precioIVAEntry = Entry(mainframe, width= 7, textvariable = self.precioIVA)
+        self.precioIVAEntry.grid(column=4, row=6, sticky=(W, E))
+        self.boton3 = Button(mainframe, text="Calcular", command = self.calcular).grid(column=2, row=7, sticky=W)
+        self.boton4 = Button(mainframe, text='Salir', command=self.__ventana.destroy, fg='black', bg = '#F85939').grid(column=4, row=7,sticky=W)
         for child in mainframe.winfo_children():
-            child.grid_configure(padx=5,pady=5)
+            child.grid_configure(padx=10, pady=10)
 
+        self.precioEntry.focus()
         self.__ventana.mainloop()
-
-    def convertir(self,*args):
-        url = 'https://www.dolarsi.com/api/api.php?type=dolar'
-        r1 = requests.get(url)
-        r2 = r1.json()
-
-        for i in range(len(r2)):
-            if r2[i]['casa']['nombre'] == 'Oficial':
-                precioVenta=r2[i]['casa']['venta']
-
-        precioVenta=float(precioVenta.replace(',','.'))
-
-        if self.dolaresEntry.get() != '':
-            try:
-                dolares = float(self.dolaresEntry.get())
-                self.__pesos.set(precioVenta*dolares)
-            except ValueError:
-                messagebox.showerror(title = 'Error de tipo', message = 'Debe ingresar un valor numérico')
-                self.__dolares.set('')
-                self.dolaresEntry.focus()
-        else:
-            self.__pesos.set('')
-
-if __name__ == '__main__':
-    app = Aplicacion()
